f906217d671e247d8daa77b50b53c5c645dbc152
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-10-12 18:43:27 +0530

    Implement: printData() in EmployeePayrollFileIOService

--- src/main/java/EmployeePayrollFileIOService.java
+++ src/main/java/EmployeePayrollFileIOService.java
@@ -18,6 +18,14 @@ public class EmployeePayrollFileIOService {
         }
     }
 
+    public static void printData() {
+        try {
+            Files.lines(new File(PAYROLL_FILE_NAME).toPath()).forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static long countEntries() {
         long entries = 0;
         try {
