abada8c6154aac5f955aef1827fffa9fdf656852
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-10-30 18:07:46 +0530

    add custom exception

---
+++ src/main/java/com/capgemini/assignment/payrole/exception/EmployeePayrollException.java
@@ -0,0 +1,17 @@
+package com.capgemini.assignment.payrole.exception;
+
+public class EmployeePayrollException extends Exception{
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 5145148817870358520L;
+
+    public EmployeePayrollException() {
+    }
+
+    public EmployeePayrollException(String message) {
+        super(message);
+    }
+
+}
