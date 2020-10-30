486f8559d77297c02c9534ade4c0cc46580e7e8a
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-10-30 20:52:42 +0530

    add attribute start date

--- src/main/java/com/capgemini/assignment/payrole/model/EmployeePayrollData.java
+++ src/main/java/com/capgemini/assignment/payrole/model/EmployeePayrollData.java
@@ -1,14 +1,22 @@
 package com.capgemini.assignment.payrole.model;
 
+import java.time.LocalDate;
+
 public class EmployeePayrollData {
     private int id;
     private String name;
     private double salary;
-
+    private LocalDate startDate;
+    
     public EmployeePayrollData(int id, String name, double salary) {
         this.id = id;
         this.name = name;
         this.salary = salary;
+    }
+    
+    public EmployeePayrollData(int id, String name, double salary, LocalDate startDate) {
+        this(id, name, salary);
+        this.startDate = startDate;
     }
 
     public int getId() {
@@ -34,9 +42,18 @@ public class EmployeePayrollData {
     public void setSalary(double salary) {
         this.salary = salary;
     }
+    
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
 
     @Override
     public String toString() {
         return "id='" + id + '\'' + ", name='" + name + '\'' + ", salary=" + salary;
     }
+
 }
