ac75db282babbd5037d86a44d41681bbf3fd7724
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-11-01 13:30:28 +0530

    add custom exception for database operations

---
+++ src/main/java/com/capgemini/assignment/payrole/exception/DBException.java
@@ -0,0 +1,17 @@
+package com.capgemini.assignment.payrole.exception;
+
+public class DBException extends Exception{
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = -3877857496640507365L;
+
+    public DBException() {
+    }
+
+    public DBException(String message) {
+        super(message);
+    }
+
+}
