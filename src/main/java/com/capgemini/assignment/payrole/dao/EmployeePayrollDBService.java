7977a7b6887b71e97edf84b7285d8b4a2afbde2a
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-11-03 00:14:55 +0530

    add employee and payroll using jdbc transaction

--- src/main/java/com/capgemini/assignment/payrole/dao/EmployeePayrollDBService.java
+++ src/main/java/com/capgemini/assignment/payrole/dao/EmployeePayrollDBService.java
@@ -95,13 +95,12 @@ public class EmployeePayrollDBService {
     }
 
     private void prepareStatementForEmployeeData() throws DBException {
-        String sqlUpdateSalary = "update employee_payroll set salary = ? where emp_name = ? and is_active is true";
-        String sqlSelectByName = "select * from employee_payroll where emp_name = ? and is_active is true";
-        String sqlSelectAll = "SELECT * FROM employee_payroll where is_active=1";
-        String sqlSelectDateRange = "SELECT * FROM employee_payroll WHERE is_active is true AND start_dt BETWEEN ? AND ?";
-        String sqlAddEmployee = "insert into employee_payroll (emp_name, gender, salary, start_dt) values"
-                + "(?, ?,?,?)";
-        String sqlRemoveEmployee = "UPDATE employee_payroll SET is_active=false WHERE is_active is true AND emp_name= ?";
+        String sqlUpdateSalary = "update employee set salary = ? where emp_name = ? and is_active is true";
+        String sqlSelectByName = "select * from employee where emp_name = ? and is_active is true";
+        String sqlSelectAll = "SELECT * FROM employee where is_active=1";
+        String sqlSelectDateRange = "SELECT * FROM employee WHERE is_active is true AND start_dt BETWEEN ? AND ?";
+        String sqlAddEmployee = "insert into employee (emp_name, gender, salary, start_dt) values" + "(?, ?,?,?)";
+        String sqlRemoveEmployee = "UPDATE employee SET is_active=false WHERE is_active is true AND emp_name= ?";
         try {
             connection = EmployeePayrollDBService.getConnection();
             updateStatementCache = connection.prepareStatement(sqlUpdateSalary);
@@ -161,7 +160,7 @@ public class EmployeePayrollDBService {
 
     @SuppressWarnings("unused")
     private int updateEmployeeDataUsingStatement(String name, double salary) throws DBException {
-        String sql = String.format("update employee_payroll set salary = %.2f where emp_name = '%s'", salary, name);
+        String sql = String.format("update employee set salary = %.2f where emp_name = '%s'", salary, name);
         try (Connection connection = EmployeePayrollDBService.getConnection()) {
             Statement statement = connection.createStatement();
             return statement.executeUpdate(sql);
@@ -205,7 +204,7 @@ public class EmployeePayrollDBService {
         if (connection == null)
             prepareStatementForEmployeeData();
 
-        String sql = "select gender, avg(salary) as average_salary from employee_payroll group by gender";
+        String sql = "select gender, avg(salary) as average_salary from employee group by gender";
         Map<String, Double> genderToAverageSalaryMap = new HashMap<>();
         try (Statement statement = connection.createStatement()) {
 
@@ -220,7 +219,7 @@ public class EmployeePayrollDBService {
         return genderToAverageSalaryMap;
     }
 
-    public boolean addEmployeePayroll(EmployeePayrollData employeePayrollData) throws DBException {
+    private boolean addEmployee(EmployeePayrollData employeePayrollData) throws DBException {
         if (addStatementCache == null)
             prepareStatementForEmployeeData();
         try {
@@ -235,15 +234,73 @@ public class EmployeePayrollDBService {
         }
     }
 
+    public boolean addEmployeePayroll(EmployeePayrollData employeePayrollData) throws DBException {
+        if (addStatementCache == null)
+            prepareStatementForEmployeeData();
+        try {
+            connection.setAutoCommit(false);
+        } catch (SQLException e2) {
+            // TODO Auto-generated catch block
+            e2.printStackTrace();
+            return false;
+        }
+        int id = -1;
+        String name = employeePayrollData.getName();
+        // add into employee table
+        try {
+            addEmployee(employeePayrollData);
+            id = getEmployeePayrollData(name).get(0).getId();
+        } catch (DBException e) {
+            try {
+                connection.rollback();
+            } catch (SQLException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
+            throw new DBException("Failed to add into employee table: " + e.getMessage());
+        }
+        // add into payroll table
+        try (Statement statement = connection.createStatement()) {
+            String sql = String.format("insert into payroll (emp_id,basic_pay) values ('%s','%s')", String.valueOf(id),
+                    String.valueOf(employeePayrollData.getSalary()));
+            int updateCount = statement.executeUpdate(sql);
+            if (updateCount < 1)
+                connection.rollback();
+
+        } catch (SQLException e) {
+            try {
+                connection.rollback();
+                connection.setAutoCommit(true);
+            } catch (SQLException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
+            throw new DBException("Failed to add into payroll table: " + e.getMessage());
+        }
+        try {
+            connection.commit();
+            connection.setAutoCommit(true);
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return true;
+
+    }
+
     public int setInactive(String name) throws DBException {
         if (removeStatementCache == null)
             prepareStatementForEmployeeData();
 
         try {
             removeStatementCache.setString(1, name);
-            return removeStatementCache.executeUpdate();
+            int result = removeStatementCache.executeUpdate();
+            return result;
         } catch (SQLException e) {
             throw new DBException("Failed to read: " + e.getMessage());
         }
+
     }
+
 }
